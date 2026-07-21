04d4d2dfc3fbe73871d6fa0fa5288659019ffd0b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-07-21 17:53:06 -0300

    a

--- src/fs/mkdir.c
+++ src/fs/mkdir.c
@@ -126,7 +126,7 @@ int create_directory(const char* const directory) {
 	#if defined(_WIN32)
 		int omit_next = isabsolute(directory);
 	#endif
-	__android_log_vprint(ANDROID_LOG_DEBUG, "libnz", "%s", directory);
+	__android_log_print(ANDROID_LOG_DEBUG, "libnz", "%s", directory);
 	len = strlen(directory) + 1;
 	
 	for (index = 1; index < len; index++) {
